8ea1930ae9048c3f5f1422a49446513042f9b4a7
Abiel Zulio M <abielzm@gmail.com>
2022-10-15 12:48:44 +0700

    feat: add static data product

---
+++ data/product.ts
@@ -0,0 +1,22 @@
+/* Static data about product */
+
+interface Product {
+  name: string
+  description: string
+  keywords: string
+  url: string
+  img: string
+  author: string
+  favicon: string
+}
+
+export const product: Product = {
+  name: "Editpasfoto",
+  description:
+    "Editpasfoto merupakan aplikasi swaedit pas foto berbasis web dengan fitur mengganti ratio ukuran foto dan menambah layer almamater kampus & jas formal",
+  keywords: "edit pas foto, pas foto, ganti ratio, almamater, jas formal",
+  url: "https://editpasfoto.com",
+  img: "/img/og.png",
+  author: "Abiel Zulio M",
+  favicon: "/img/favicon.ico",
+}
