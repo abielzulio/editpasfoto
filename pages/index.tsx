9f5fef991d05b438c66ed7d68afa7a8da5607aa3
Abiel Zulio M <abielzm@gmail.com>
2022-10-15 13:48:10 +0700

    feat: hello world main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from "next"
 
 const Home: NextPage = () => {
-  return <div>Hello world</div>
+  return (
+    <section className="w-screen h-screen text-center flex flex-col align-center justify-center bg-black-radial-gradient">
+      <h1 className="text-[54px] text-transparent bg-white-linear-gradient bg-clip-text tracking-tight leading-[54px]">
+        Hello, world
+      </h1>
+    </section>
+  )
 }
 
 export default Home
