6fc70f7ae8b7e74a22b0cdfeeedb86de3fedadde
Abiel Zulio M <abielzm@gmail.com>
2022-10-16 20:16:10 +0700

    fix: new copy

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,8 @@ import type { NextPage } from "next"
 const Home: NextPage = () => {
   return (
     <section className="w-screen h-screen py-[100px] text-center flex flex-col align-center justify-center bg-black-radial-gradient">
-      <h1 className="text-[96px] text-transparent bg-white-linear-gradient bg-clip-text tracking-tight leading-[96px]">
-        editpasfoto.com
+      <h1 className="text-[64px] text-transparent bg-white-linear-gradient bg-clip-text tracking-tight leading-[64px]">
+        Edit pas foto. <br /> Tanpa skill edit foto.
       </h1>
     </section>
   )
