5428f18932bcadd0a1d40b315e12c0b24441cf2a
Abiel Zulio M <abielzm@gmail.com>
2022-10-16 20:07:18 +0700

    fix: add hero

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,9 @@ import type { NextPage } from "next"
 
 const Home: NextPage = () => {
   return (
-    <section className="w-screen h-screen text-center flex flex-col align-center justify-center bg-black-radial-gradient">
-      <h1 className="text-[54px] text-transparent bg-white-linear-gradient bg-clip-text tracking-tight leading-[54px]">
-        Hello, world
+    <section className="w-screen h-screen py-[100px] text-center flex flex-col align-center justify-center bg-black-radial-gradient">
+      <h1 className="text-[96px] text-transparent bg-white-linear-gradient bg-clip-text tracking-tight leading-[96px]">
+        editpasfoto.com
       </h1>
     </section>
   )
