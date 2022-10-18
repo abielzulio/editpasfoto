fdf991a32dfc5bc5860a4a3a46bcbb94335c151e
Abiel Zulio M <abielzm@gmail.com>
2022-10-18 22:49:13 +0700

    feat: add hero component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,12 @@
 import Dropzone from "components/Dropzone"
 import Author from "components/Author"
+import Hero from "components/Hero"
 import type { NextPage } from "next"
 
 const Home: NextPage = () => {
   return (
     <section className="min-w-screen min-h-screen md:py-[100px] py-[50px] text-center flex flex-col align-center justify-center">
-      <h1 className="md:text-[64px] text-[36px] text-transparent bg-white-linear-gradient bg-clip-text tracking-tight md:leading-[64px] leading-[36px]">
-        Edit pas foto. <br /> Tanpa skill edit foto.
-      </h1>
+      <Hero />
       <Dropzone />
       <Author />
     </section>
