4591cfc4c6811135e54ca0f0948c74537b1629fd
Abiel Zulio M <abielzm@gmail.com>
2022-10-19 13:35:29 +0700

    refactor: dropzone, editor, tool

--- components/Dropzone/Dropzone.tsx
+++ components/Dropzone/Dropzone.tsx
@@ -1,35 +1,16 @@
-import { useCallback } from "react"
-import { useDropzone } from "react-dropzone"
-import { useState } from "react"
-import { Image } from "types"
-import Editor from "components/Editor"
-import { SAMPLE_IMAGE } from "data/editor"
 import {
   CheckCircledIcon,
-  CheckIcon,
   CrossCircledIcon,
   UploadIcon,
 } from "@radix-ui/react-icons"
+import { BASE_SIZE, IMAGE_SCALER_FACTOR } from "data/editor"
+import { useDropzone } from "react-dropzone"
 
-const Dropzone = () => {
-  const [image, setImage] = useState<Image[]>([])
+interface DropzoneProps {
+  onDropAccepted: (acceptedFiles: File[]) => void
+}
 
-  const onDropAccepted = useCallback((acceptedFiles: File[]) => {
-    acceptedFiles.map((file: File, index: number) => {
-      const reader: FileReader = new FileReader()
-      reader.onabort = () => console.log("Pembacaan foto dibatalkan")
-      reader.onerror = () => console.log("Pembacaan foto gagal")
-      reader.onload = () => {
-        setImage((prev) => [
-          ...prev,
-          { id: index, src: reader.result?.toString(), name: file.name },
-        ])
-      }
-      reader.readAsDataURL(file)
-      return file
-    })
-  }, [])
-
+const Dropzone = (props: DropzoneProps) => {
   const {
     getRootProps,
     getInputProps,
@@ -41,53 +22,38 @@ const Dropzone = () => {
       "image/*": [".jpg", ".jpeg", ".png"],
     },
     maxFiles: 1,
-    onDropAccepted: onDropAccepted,
+    onDropAccepted: props.onDropAccepted,
   })
 
   return (
-    <section className="h-min my-[60px] mx-auto bg-white bg-opacity-5 p-[25px] border-gray-50 border-[1px] border-opacity-10 rounded-xl">
-      {image.length > 0 && <Editor image={image} resetImage={setImage} />}
-      {image.length == 0 && (
-        <>
-          <p className="text-white mb-[25px] text-opacity-80">
-            Unggah pas foto
-          </p>
-          <div
-            {...getRootProps({ className: "dropzone" })}
-            className={`transition flex flex-col justify-center items-center border-opacity-20 border-[2px] p-[25px] text-white w-[150px] h-[200px] rounded-md text-opacity-30 hover:border-opacity-50 hover:cursor-pointer hover:bg-gray-800 hover:text-opacity-100 ${
-              isDragAccept
-                ? "border-opacity-100 text-opacity-100 bg-gray-800 border-green-400 !text-green-400"
-                : "border-dashed"
-            } ${
-              isDragReject
-                ? "border-red-400 border-opacity-100 !text-red-400 cursor-no-drop border-solid"
-                : "border-dashed border-white border-opacity-20"
-            }`}
-          >
-            <input
-              {...getInputProps()}
-              className={`w-full h-full ${
-                isDragReject ? "cursor-not-allowed" : ""
-              }`}
-            />
-            {!isDragActive && <UploadIcon width={18} height={18} />}
-            {isDragAccept && <CheckCircledIcon width={18} height={18} />}
-            {isDragReject && <CrossCircledIcon width={18} height={18} />}
-            <p className="text-sm my-[12px]">
-              {!isDragActive && `Tarik file atau klik untuk mengunggah`}
-              {isDragAccept && `Foto dapat diunggah`}
-              {isDragReject && `Hanya mendukung .png, .jpg, dan .jpeg`}
-            </p>
-          </div>
-          <button
-            onClick={() => setImage(SAMPLE_IMAGE)}
-            className="mt-[20px] text-opacity-50 text-white text-sm hover:text-opacity-80 transition"
-          >
-            Lihat contoh
-          </button>
-        </>
-      )}
-    </section>
+    <div
+      {...getRootProps({ className: "dropzone" })}
+      className={`transition flex w-full h-full flex-col justify-center items-center border-opacity-20 border-[2px] p-[25px] text-white rounded-md text-opacity-30 hover:border-opacity-50 hover:cursor-pointer hover:bg-gray-800 hover:text-opacity-100 ${
+        isDragAccept
+          ? "border-opacity-100 text-opacity-100 bg-gray-800 border-green-400 !text-green-400"
+          : "border-dashed"
+      } ${
+        isDragReject
+          ? "border-red-400 border-opacity-100 !text-red-400 cursor-no-drop border-solid"
+          : "border-dashed border-white border-opacity-20"
+      }`}
+      style={{
+        width: `${BASE_SIZE * IMAGE_SCALER_FACTOR}px`,
+      }}
+    >
+      <input
+        {...getInputProps()}
+        className={`w-full h-full ${isDragReject ? "!cursor-not-allowed" : ""}`}
+      />
+      {!isDragActive && <UploadIcon width={18} height={18} />}
+      {isDragAccept && <CheckCircledIcon width={18} height={18} />}
+      {isDragReject && <CrossCircledIcon width={18} height={18} />}
+      <p className="text-md my-[12px]">
+        {!isDragActive && `Tarik foto ke sini atau klik untuk mengunggah`}
+        {isDragAccept && `Foto dapat diunggah`}
+        {isDragReject && `Hanya mendukung .png, .jpg, dan .jpeg`}
+      </p>
+    </div>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import Dropzone from "components/Dropzone"
 import Author from "components/Author"
+import Editor from "components/Editor"
 import Hero from "components/Hero"
 import type { NextPage } from "next"
 
@@ -7,7 +7,7 @@ const Home: NextPage = () => {
   return (
     <section className="min-w-screen min-h-screen py-[30px] text-center flex flex-col align-center justify-center">
       <Hero />
-      <Dropzone />
+      <Editor />
       <Author />
     </section>
   )
