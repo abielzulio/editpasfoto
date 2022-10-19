f991a7b5674c55d1c865d41b67eb34a3f99a4376
Abiel Zulio M <abielzm@gmail.com>
2022-10-19 15:52:12 +0700

    refactor

--- data/photo.ts
+++ data/photo.ts
@@ -1,5 +1,20 @@
 import { Ratio } from "types"
 
+export const PHOTO_Y_AXIS_MIN: number = -200
+export const PHOTO_Y_AXIS_MAX: number = 200
+export const PHOTO_Y_AXIS_STEP: number = 0.05
+export const PHOTO_Y_AXIS_DEFAULT: number = 0
+
+export const PHOTO_X_AXIS_MIN: number = -200
+export const PHOTO_X_AXIS_MAX: number = 200
+export const PHOTO_X_AXIS_STEP: number = 0.05
+export const PHOTO_X_AXIS_DEFAULT: number = 0
+
+export const PHOTO_SCALE_MIN: number = 0
+export const PHOTO_SCALE_MAX: number = 3
+export const PHOTO_SCALE_STEP: number = 0.05
+export const PHOTO_SCALE_DEFAULT: number = 1
+
 export const PHOTO_BASE_SIZE: number = 300
 
 export const PHOTO_RATIO_OPTIONS: Ratio[] = [
@@ -28,18 +43,3 @@ export const PHOTO_RATIO_OPTIONS: Ratio[] = [
     height: PHOTO_BASE_SIZE,
   },
 ]
-
-export const PHOTO_Y_AXIS_MIN: number = -200
-export const PHOTO_Y_AXIS_MAX: number = 200
-export const PHOTO_Y_AXIS_STEP: number = 0.05
-export const PHOTO_Y_AXIS_DEFAULT: number = 0
-
-export const PHOTO_X_AXIS_MIN: number = -200
-export const PHOTO_X_AXIS_MAX: number = 200
-export const PHOTO_X_AXIS_STEP: number = 0.05
-export const PHOTO_X_AXIS_DEFAULT: number = 0
-
-export const PHOTO_SCALE_MIN: number = 0
-export const PHOTO_SCALE_MAX: number = 3
-export const PHOTO_SCALE_STEP: number = 0.05
-export const PHOTO_SCALE_DEFAULT: number = 1
