35a9ca07535aadf5ecf0b5a2a122f84825e07659
Abiel Zulio M <abielzm@gmail.com>
2022-10-20 12:47:50 +0700

    refactor: use local author img asset

--- data/author.ts
+++ data/author.ts
@@ -1,4 +1,3 @@
 export const AUTHOR_NAME: string = "Abiel Zulio M"
 export const AUTHOR_URL: string = "https://twitter.com/abielzulio"
-export const AUTHOR_IMG_URL: string =
-  "https://pbs.twimg.com/profile_images/1563752769265487872/SOkzDKgH_400x400.jpg"
+export const AUTHOR_IMG_URL: string = "/img/author.jpg"
--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    domains: ["pbs.twimg.com"],
-  },
 }
 
 module.exports = nextConfig
